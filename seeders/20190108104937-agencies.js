281592df0ce26b16905b46d6d46a17fee733f8ef
GregoireMorane <gh.morane@gmail.com>
2019-01-18 11:38:19 +0100

    test testest prod deploy heroku seeders

--- seeders/20190108104937-agencies.js
+++ seeders/20190108104937-agencies.js
@@ -1,5 +1,4 @@
 'use strict';
-process.env.NODE_ENV === 'production';
 const model = require('../models');
 
 module.exports = {
--- seeders/20190108104959-email_extensions.js
+++ seeders/20190108104959-email_extensions.js
@@ -1,5 +1,4 @@
 'use strict';
-process.env.NODE_ENV === 'production';
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
